4697a875377c068367afeed48f93856a718af6f4
lgrees <lukasgrees@gmail.com>
2018-11-14 11:41:05 -0600

    fixes unit test for tag model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,16 +5,16 @@ const Equipment = require('./equipment')
 const Tag = require('./tag')
 
 //Tag to Recipe relationship
-Recipe.hasMany(Tag, {through: 'RecipeTags'})
-Tag.hasMany(Recipe, {through: 'RecipeTags'})
+Recipe.belongsToMany(Tag, {through: 'RecipeTags'})
+Tag.belongsToMany(Recipe, {through: 'RecipeTags'})
 
 //Ingredient to Recipe relationship
-Recipe.hasMany(Ingredient, {through: 'RecipeIngredients'})
-Ingredient.hasMany(Recipe, {through: 'RecipeIngredients'})
+Recipe.belongsToMany(Ingredient, {through: 'RecipeIngredients'})
+Ingredient.belongsToMany(Recipe, {through: 'RecipeIngredients'})
 
 //Equipment to Recipe relationship
-Equipment.hasMany(Recipe, {through: 'RecipeEquipment'})
-Recipe.hasMany(Equipment, {through: 'RecipeEquipment'})
+Equipment.belongsToMany(Recipe, {through: 'RecipeEquipment'})
+Recipe.belongsToMany(Equipment, {through: 'RecipeEquipment'})
 
 module.exports = {
   User,
