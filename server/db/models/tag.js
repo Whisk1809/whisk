2971bc39756370b9d7b031a81a73b5890b695684
lgrees <lukasgrees@gmail.com>
2018-11-14 10:11:52 -0600

    adds a tag model

---
+++ server/db/models/tag.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Tag = db.define('tag', {
+  name: {
+    type: Sequelize.STRING,
+    validate: {notEmpty: true},
+    allowNull: false
+  },
+})
+
+//
+
+module.exports = Tag
