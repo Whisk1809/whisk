f77ef2cede04a3a0ee7d730bce42c38cca689ce6
lgrees <lukasgrees@gmail.com>
2018-11-14 09:36:45 -0600

    adds beginner recipes model

--- server/db/models/recipe.js
+++ server/db/models/recipe.js
@@ -1,1 +1,27 @@
-//test
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Recipe = db.define('recipe', {
+  title: {
+    type: Sequelize.STRING,
+    validate: {notEmpty: true},
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'default-recipe.jpg'
+  },
+  directions: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    validate: {},
+    allowNull: false
+  }
+})
+
+//
+
+module.exports = Recipe
