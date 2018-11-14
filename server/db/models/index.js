e0e27b10aa84e214eb8e218eb7ec1073cb938ce3
Dilan Ozusta <dilanozusta@outlook.com>
2018-11-14 11:04:49 -0600

    model relationships defined

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,20 +1,20 @@
 const User = require('./user')
 const Recipe = require('./recipe')
+const Ingredient = require('./ingredient')
+const Equipment = require('./equipment')
+const Tag = require('./tag')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Recipe.hasMany(Tag, {through: 'RecipeTags'})
+Tag.hasMany(Recipe, {through: 'RecipeTags'})
+Recipe.hasMany(Ingredient, {through: 'RecipeIngredients'})
+Ingredient.hasMany(Recipe, {through: 'RecipeIngredients'})
+Equipment.hasMany(Recipe, {through: 'RecipeEquipment'})
+Recipe.hasMany(Equipment, {through: 'RecipeEquipment'})
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
   User,
-  Recipe
+  Recipe,
+  Ingredient,
+  Equipment,
+  Tag
 }
