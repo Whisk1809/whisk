4e1a40bc0d9dba099cba358d5024dbf9da1f4804
lgrees <lukasgrees@gmail.com>
2018-11-14 11:21:37 -0600

    minor formatting

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,10 +4,15 @@ const Ingredient = require('./ingredient')
 const Equipment = require('./equipment')
 const Tag = require('./tag')
 
+//Tag to Recipe relationship
 Recipe.hasMany(Tag, {through: 'RecipeTags'})
 Tag.hasMany(Recipe, {through: 'RecipeTags'})
+
+//Ingredient to Recipe relationship
 Recipe.hasMany(Ingredient, {through: 'RecipeIngredients'})
 Ingredient.hasMany(Recipe, {through: 'RecipeIngredients'})
+
+//Equipment to Recipe relationship
 Equipment.hasMany(Recipe, {through: 'RecipeEquipment'})
 Recipe.hasMany(Equipment, {through: 'RecipeEquipment'})
 
