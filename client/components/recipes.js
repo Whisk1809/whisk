1a19ac816bad6fdf8b4ddfb79bf84f88f73ed575
Patrick <pdylanconroy@gmail.com>
2018-11-14 10:17:26 -0600

    initial commit

---
+++ client/components/recipes.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import {getAllRecipes} from "../store/recipes"
+import { List } from 'semantic-ui-react'
+
+class Recipes extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <List>
+          {this.props.recipes.map(recipe => {
+            return (
+
+            )
+          })}
+          </List>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    recipes: state.recipes
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllRecipes: dispatch(getAllRecipes())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Recipes);
