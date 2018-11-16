52ee7c8e760722c9ef051e6250195f011f07f85f
lgrees <lukasgrees@gmail.com>
2018-11-16 08:33:05 -0600

    fixes unique constraint

--- server/db/graphDb.js
+++ server/db/graphDb.js
@@ -21,26 +21,62 @@ async function runQuery(cypher, params = {}) {
 const deleteGraph = async () => {
   await runQuery(`match (n) detach delete n`)
 }
+const createConstraints = async () => {
+  await runQuery(`CREATE CONSTRAINT ON(u: User) ASSERT u.pk IS UNIQUE`)
+  await runQuery(`CREATE CONSTRAINT ON(r: Recipe) ASSERT r.pk IS UNIQUE`)
+}
 
 //verify that user and entity exist in graph db, create / update relationship between them
-const like = async (userId, entity) => {
+//TODO consider case where user switches from liking to disliking an entity (or vice versa)
+
+const likeRecipe = async (userId, recipeId) => {
   await runQuery(
-    ` merge (u:User { pk: {userPk}})-[:likes]->(r:Recipe {pk: {recipePk}})`,
-    {userPk: userId.toString(), recipePk: entity.id.toString()}
+    // `MERGE (u:User { pk: {userPk}})-[:likes]->(r:Recipe {pk: {recipePk}})`,
+    `merge (u:User {pk:{userPk} })
+    merge (r:Recipe {pk:{recipePk} })
+    merge (u)-[l:likes]->(r)`,
+    {
+      userPk: userId.toString(),
+      recipePk: recipeId.toString()
+    }
   )
 }
-
-like(1, {id: 1, type: 'Recipe'})
-
-const dislike = (userId, entity) => {
-  //verify that user and entity exist in graph db, create / update relationship between them
+const dislikeRecipe = async (userId, recipeId) => {
+  await runQuery(
+    ` merge (u:User { pk: {userPk}})-[:dislikes]->(r:Recipe {pk: {recipePk}})`,
+    {userPk: userId.toString(), recipePk: recipeId.toString()}
+  )
 }
-const recommend = userId => {
-  //return an array of recommended recipes
+const likeCategory = async (userId, categoryId) => {
+  await runQuery(
+    ` merge (u:User { pk: {userPk}})-[:likes]->(r:Category {pk: {categoryPk}})`,
+    {userPk: userId.toString(), categoryPk: categoryId.toString()}
+  )
 }
+const dislikeCategory = async (userId, categoryId) => {
+  await runQuery(
+    ` merge (u:User { pk: {userPk}})-[:dislikes]->(r:Category {pk: {categoryPk}})`,
+    {userPk: userId.toString(), categoryPk: categoryId.toString()}
+  )
+}
+;(async () => {
+  await deleteGraph()
+  await createConstraints()
+  await likeRecipe(1, 1)
+  await likeRecipe(1, 2)
+  await likeRecipe(1, 3)
+  await likeRecipe(2, 1)
+  await likeRecipe(2, 3)
+  await likeRecipe(3, 1)
+  await likeRecipe(3, 3)
+})()
 
-module.exports = {
-  like,
-  dislike,
-  recommend
-}
+// const recommend = userId => {
+//   //return an array of recommended recipes
+// }
+
+// module.exports = {
+//   like,
+//   dislike,
+//   recommend
+// }
