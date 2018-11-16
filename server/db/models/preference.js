f1be1d1d5b649789c5bb6dc774d752c5f619524d
lgrees <lukasgrees@gmail.com>
2018-11-16 14:55:35 -0600

    adds after create preferences hook

--- server/db/models/preference.js
+++ server/db/models/preference.js
@@ -1,12 +1,23 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const {like, dislike} = require('../graphDb')
 
 const Preference = db.define('preference', {
-  prefers:{
+  prefers: {
     type: Sequelize.BOOLEAN,
     allowNull: false
-  },
-
+  }
 })
+
+Preference.afterCreate(preference => {
+  const {userId, recipeId, ingredientId, prefers} = preference
+  const entityToPrefer = {entityId: 1, entity: 'Recipe'}
+  if (prefers) {
+    like(userId, entityToPrefer)
+  } else {
+    dislike(userId, entityToPrefer)
+  }
+})
+Preference.afterUpdate()
 
 module.exports = Preference
