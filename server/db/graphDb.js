7cebdd614ff20147a3293f34787864c266cc92a6
lgrees <lukasgrees@gmail.com>
2018-11-17 15:46:06 -0600

    successfully computes jaccard index for 2 users

--- server/db/graphDb.js
+++ server/db/graphDb.js
@@ -1,5 +1,6 @@
 //establish db connection
 const neo4j = require('neo4j-driver').v1
+const _ = require('lodash')
 
 const driver = neo4j.driver(
   'bolt://localhost:7687',
@@ -83,9 +84,102 @@ const dislikeIngredient = async (userId, ingredientId) => {
     {userPk: userId.toString(), ingredientPk: ingredientId.toString()}
   )
 }
+
+// computeLikeIntersection (L1 intersect L2)
+
+const computeLikeIntersection = async (uId1, uId2) => {
+  const intersect = await runQuery(
+    `MATCH (u1:User {pk:{uId1}})-[:likes]->(n)<-[:likes]-(u2:User {pk:{uId2}})
+    RETURN n
+    `,
+    {uId1: uId1.toString(), uId2: uId2.toString()}
+  )
+  return intersect.records.length
+}
+//given a node, returns the pk (as a string)
+const pk = node => {
+  return node._fields[0].properties.pk
+}
+
+// computeInteractionUnion (L1 union L2 union D1 union D2)
+// is it pretty? no. does it work? yes.
+const computeInteractionUnion = async (uId1, uId2) => {
+  const [l1, l2, d1, d2] = await Promise.all([
+    runQuery(
+      `MATCH (u1:User {pk:{uId1}})-[:likes]->(n)
+    RETURN n`,
+      {uId1: uId1.toString()}
+    ),
+    runQuery(
+      `MATCH (u1:User {pk:{uId2}})-[:likes]->(n)
+    RETURN n`,
+      {uId2: uId2.toString()}
+    ),
+    runQuery(
+      `MATCH (u1:User {pk:{uId1}})-[:dislikes]->(n)
+    RETURN n`,
+      {uId1: uId1.toString()}
+    ),
+    runQuery(
+      `MATCH (u1:User {pk:{uId2}})-[:dislikes]->(n)
+    RETURN n`,
+      {uId2: uId2.toString()}
+    )
+  ])
+  const l1Pks = l1.records.map(record => pk(record))
+  const l2Pks = l2.records.map(record => pk(record))
+  const d1Pks = d1.records.map(record => pk(record))
+  const d2Pks = d2.records.map(record => pk(record))
+  const union = _.union(l1Pks, l2Pks, d1Pks, d2Pks)
+
+  return union.length
+}
+
+// computeDislikeIntersection (D1 intersect D2)
+
+const computeDislikeIntersection = async (uId1, uId2) => {
+  const intersect = await runQuery(
+    `MATCH (u1:User {pk:{uId1}})-[:dislikes]->(n)<-[:dislikes]-(u2:User {pk:{uId2}})
+      RETURN n
+      `,
+    {uId1: uId1.toString(), uId2: uId2.toString()}
+  )
+  return intersect.records.length
+}
+
+// computeMismatchIntersection (L1 intersect D2 + D1 intersect L2)
+const computeMismatchIntersection = async (uId1, uId2) => {
+  const intersect1 = await runQuery(
+    `MATCH (u1:User {pk:{uId1}})-[:likes]->(n)<-[:dislikes]-(u2:User {pk:{uId2}})
+        RETURN n
+        `,
+    {uId1: uId1.toString(), uId2: uId2.toString()}
+  )
+  const intersect2 = await runQuery(
+    `MATCH (u1:User {pk:{uId1}})-[:dislikes]->(n)<-[:likes]-(u2:User {pk:{uId2}})
+        RETURN n
+        `,
+    {uId1: uId1.toString(), uId2: uId2.toString()}
+  )
+  return intersect1.records.length + intersect2.records.length
+}
+
+//given two user IDs, computes the modified Jaccard Index (a similarity index between -1 and 1)
+
+const computeJaccard = async (uId1, uId2) => {
+  const [sameLikes, sameDislikes, mismatches, union] = await Promise.all([
+    computeLikeIntersection(uId1, uId2),
+    computeDislikeIntersection(uId1, uId2),
+    computeMismatchIntersection(uId1, uId2),
+    computeInteractionUnion(uId1, uId2)
+  ])
+  const jaccard = (sameLikes + sameDislikes - mismatches) / union
+  return jaccard
+}
 ;(async () => {
-  await deleteGraph()
-  await createConstraints()
+  // await deleteGraph()
+  // await createConstraints()
+  await computeJaccard(1, 2)
 })()
 
 // const recommend = userId => {
