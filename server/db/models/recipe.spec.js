ec8a2a324d06c1abfb981cbfc747f253a35c724c
sacreutz <sacreutz@gmail.com>
2018-11-19 11:36:32 -0600

    fixed tests

--- server/db/models/recipe.spec.js
+++ server/db/models/recipe.spec.js
@@ -6,20 +6,15 @@ describe('Recipe model', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
-  it('has fields title, description, prepTime, imageUrl, directions', () => {
+  it('has fields title, description, prepTime, imageUrl', () => {
     const recipe = Recipe.build({
       title: 'Grilled Cheese',
       description: 'A really mediocre sandwich',
       prepTime: '10 minutes',
-      directions:
-        '1. melt butter in heated pan 2. assemble sandwich 3. Grill it!'
     })
     expect(recipe.title).to.equal('Grilled Cheese')
     expect(recipe.description).to.equal('A really mediocre sandwich')
     expect(recipe.prepTime).to.equal('10 minutes')
-    expect(recipe.directions).to.equal(
-      '1. melt butter in heated pan 2. assemble sandwich 3. Grill it!'
-    )
     expect(recipe.imageUrl).to.equal('default-recipe.jpg')
   })
 
