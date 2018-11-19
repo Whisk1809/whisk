81acd1ea13a36f856ef5eef408d8c6022abd15f3
sacreutz <sacreutz@gmail.com>
2018-11-19 16:31:43 -0600

    fixed

--- send_sms.js
+++ send_sms.js
@@ -1,5 +1,4 @@
-const accountSid = 'AC73c8fa517d3e83ccc4c9c6897586ce8e';
-const authToken = '9622761b850dfc615521b37b1266db99';
+const {accountSid, authToken} = require('./secrets')
 const client = require('twilio')(accountSid, authToken);
 //const schedule = require('node-schedule')
 var CronJob = require('cron').CronJob;
@@ -10,7 +9,7 @@ function morningText() {
 
   client.messages
     .create({
-       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+       body: 'What do you want to cook today?',
        from: '+18064244869',
        to: '+13364136015'
      })
@@ -20,11 +19,32 @@ function morningText() {
 
 }
 
+function eveningText() {
+
+  client.messages
+    .create({
+      body: 'Time to start cooking!',
+      from: '+18064244869',
+      to: '+13364136015'
+    })
+    .then(message => console.log(message.sid))
+    .done();
+}
+
+//modify the function below so that instead of console logging, it runs morningText function at a specific time in the morning
 console.log('before job')
-const job = new CronJob('* * * * * *', function() {
+const job = new CronJob('30 8 * * *', function() {
   console.log('You will see this message ');
+  morningText()
 });
 console.log('after job instantiation')
 job.start()
 
 console.log('job started')
+
+const job2 = new CronJob('30 17 * * *', function() {
+  console.log('See job2')
+  eveningText()
+});
+job2.start()
+console.log('job2 started')
