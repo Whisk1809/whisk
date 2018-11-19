bfdfcfd249051f1f7d51983fa34289975d405f27
lgrees <lukasgrees@gmail.com>
2018-11-19 16:19:12 -0600

    adds functional yummly data fetcher

--- server/adapter/yummly-data-fetch.js
+++ server/adapter/yummly-data-fetch.js
@@ -1,9 +1,11 @@
 const _app_id = '17a7b50a'
 const _app_key = '24c0fdf7b4d6c17c0032b72e425362fe'
 const axios = require('axios')
+const fs = require('fs')
 
 const testTerms = ['burrito', 'pizza']
 const terms = [
+  'christmas',
   'burrito',
   'pizza',
   'sandwich',
@@ -34,7 +36,8 @@ const getRecipe = async recipeId => {
     const uri = `http://api.yummly.com/v1/api/recipe/${recipeId}`
     const params = {_app_id, _app_key}
     const {data} = await axios.get(uri, {params})
-    console.log(data)
+    // console.log(data)
+    return data
   } catch (err) {
     console.error(err)
   }
@@ -54,10 +57,9 @@ const searchRecipes = async searchTerm => {
       _app_key,
       q: searchTerm,
       requirePictures: true,
-      maxResult: 10
+      maxResult: 100
     }
     const {data} = await axios.get(uri, {params})
-    console.log(data)
     return data
   } catch (err) {
     console.error(err)
@@ -78,11 +80,25 @@ const fetchData = async () => {
     for (let j = 0; j < recipeTermData[i].length; j++) {
       const recipeData = recipeTermData[i][j]
       const singleRecipeData = await getRecipe(recipeTermData[i][j].id)
+      yummlyData.push({
+        sourceId: recipeData.id,
+        source: 'YUMMLY',
+        prepTimeSeconds: recipeData.totalTimeInSeconds,
+        totalTimeInSeconds: singleRecipeData.totalTimeInSeconds,
+        prepTime: singleRecipeData.prepTime,
+        numberOfServings: singleRecipeData.numberOfServings,
+        sourceRecipeUrl: singleRecipeData.source.sourceRecipeUrl,
+        ingredientList: singleRecipeData.ingredientLines,
+        ingredients: recipeData.ingredients,
+        imageUrl: singleRecipeData.images[0].hostedLargeUrl,
+        title: recipeData.recipeName,
+        cuisine: recipeData.attributes.cuisine,
+        course: recipeData.attributes.course,
+        holiday: recipeData.attributes.holiday
+      })
     }
   }
+  fs.writeFileSync(__dirname + '/yummly-data.json', JSON.stringify(yummlyData))
 }
 
-testTerms.map(async term => {
-  const {matches} = await searchRecipes(term)
-  const recipes = await Promise.all(matches.map(recipe => getRecipe(recipe.id)))
-})
+fetchData()
