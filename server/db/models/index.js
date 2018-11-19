5d780e217986057f55f3964e0175e03e0591de66
lgrees <lukasgrees@gmail.com>
2018-11-19 10:38:13 -0600

    adds recipe model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user')
 
+const Recipe = require('./recipe')
 const Ingredient = require('./ingredient')
 const Equipment = require('./equipment')
 const Tag = require('./tag')
@@ -21,16 +22,16 @@ Requirement.belongsTo(Tag)
 Requirement.belongsTo(Ingredient)
 
 // //Tag to Recipe relationship
-// Recipe.belongsToMany(Tag, {through: 'RecipeTags'})
-// Tag.belongsToMany(Recipe, {through: 'RecipeTags'})
+Recipe.belongsToMany(Tag, {through: 'RecipeTags'})
+Tag.belongsToMany(Recipe, {through: 'RecipeTags'})
 
 // //Ingredient to Recipe relationship
-// Recipe.belongsToMany(Ingredient, {through: 'RecipeIngredients'})
-// Ingredient.belongsToMany(Recipe, {through: 'RecipeIngredients'})
+Recipe.belongsToMany(Ingredient, {through: 'RecipeIngredients'})
+Ingredient.belongsToMany(Recipe, {through: 'RecipeIngredients'})
 
 // //Equipment to Recipe relationship
-// Equipment.belongsToMany(Recipe, {through: 'RecipeEquipment'})
-// Recipe.belongsToMany(Equipment, {through: 'RecipeEquipment'})
+Equipment.belongsToMany(Recipe, {through: 'RecipeEquipment'})
+Recipe.belongsToMany(Equipment, {through: 'RecipeEquipment'})
 
 module.exports = {
   User,
