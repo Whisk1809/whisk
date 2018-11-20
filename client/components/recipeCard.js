4097f1b2e5346d1a60261e6b1d833e3b8a761e73
Dilan Ozusta <dilanozusta@outlook.com>
2018-11-20 07:18:12 -0600

    card view populating data from mock normalizer data

--- client/components/recipeCard.js
+++ client/components/recipeCard.js
@@ -4,18 +4,25 @@ import {Card, Icon, Image} from 'semantic-ui-react'
 class RecipeCard extends Component {
   render() {
     const {recipe} = this.props
+
+    const handleClick = () => {
+      alert('you\'ve clicked this')
+    }
+
     return (
       <Card>
         <Image src={recipe.imageUrl} />
         <Card.Content>
-          <Card.Header>{recipe.title}</Card.Header>
+          <Card.Header onClick={handleClick}> {recipe.title}</Card.Header>
           <Card.Meta>{recipe.prepTime}</Card.Meta>
           <Card.Description>Direction tba</Card.Description>
         </Card.Content>
         <Card.Content extra>
           <a>
-            <Icon name="user" />
-            22 Friends
+            <Icon name="heart" />
+          </a>
+          <a>
+            <Icon name="ban" />
           </a>
         </Card.Content>
       </Card>
