93c19662a65e6a2ba8795ee70f3bd4936e07e888
Dilan Ozusta <dilanozusta@outlook.com>
2018-11-20 09:36:37 -0600

    redirecting link to single recipe

--- client/components/recipeCard.js
+++ client/components/recipeCard.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react'
 import {Card, Icon, Image} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 
 class RecipeCard extends Component {
   render() {
     const {recipe} = this.props
     const {id, imageUrl, title, prepTime} = recipe
 
-    const handleClick = () => {
-      this.props.history.push(`recipes/${id}`)
-    }
-
     return (
-      <Card>
+
+      <Card as={Link} to={`/recipes/${id}`}>
         <Image src={imageUrl} />
         <Card.Content>
-          <Card.Header onClick={handleClick}> {title}</Card.Header>
+          <Card.Header> {title}</Card.Header>
           <Card.Meta>{prepTime}</Card.Meta>
           <Card.Description>Direction tba</Card.Description>
         </Card.Content>
