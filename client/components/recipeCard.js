fd9f8125e7d41175c7a557f202454ba67838f7cd
sacreutz <sacreutz@gmail.com>
2018-11-21 10:10:39 -0600

    fixed merge conflict

--- client/components/recipeCard.js
+++ client/components/recipeCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Card, Icon, Image} from 'semantic-ui-react'
+import {Card, Icon, Image, Container} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 class RecipeCard extends Component {
@@ -8,23 +8,24 @@ class RecipeCard extends Component {
     const {id, imageUrl, title, prepTime} = recipe
 
     return (
-
-      <Card as={Link} to={`/recipes/${id}`}>
-        <Image src={imageUrl} />
-        <Card.Content>
-          <Card.Header> {title}</Card.Header>
-          <Card.Meta>{prepTime}</Card.Meta>
-          <Card.Description>Direction tba</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name="heart" />
-          </a>
-          <a>
-            <Icon name="ban" />
-          </a>
-        </Card.Content>
-      </Card>
+      <Container>
+        <Card as={Link} to={`/recipes/${id}`}>
+          <Image src={imageUrl} />
+          <Card.Content>
+            <Card.Header> {title}</Card.Header>
+            <Card.Meta>{prepTime}</Card.Meta>
+            <Card.Description/>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="heart" />
+            </a>
+            <a>
+              <Icon name="ban" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Container>
     )
   }
 }
