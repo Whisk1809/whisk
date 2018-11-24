c7664d8750981a10d11cfe5310200965570df1a4
Dilan Ozusta <dilanozusta@outlook.com>
2018-11-24 09:44:15 -0600

    created a dummy component for Favorites

---
+++ client/components/favorites.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { Segment, Container, Button} from 'semantic-ui-react'
+
+class Favorites extends Component {
+  render() {
+    return (
+      <Container>
+        <Segment.Group raised>
+          <Segment>
+            Recipe 1
+            <Button color="blue">remove</Button>
+          </Segment>
+          <Segment>
+            Recipe 2
+            <Button color="blue">remove</Button>
+          </Segment>
+          <Segment>
+            Recipe 3
+            <Button color="blue">remove</Button>
+          </Segment>
+        </Segment.Group>
+      </Container>
+    )
+  }
+}
+
+export default Favorites
