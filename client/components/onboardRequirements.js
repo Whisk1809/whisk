b4bf358d6a7918dc45fab2fa388d417692618484
Patrick <pdylanconroy@gmail.com>
2018-11-26 11:10:39 -0600

    now works

--- client/components/onboardRequirements.js
+++ client/components/onboardRequirements.js
@@ -162,7 +162,7 @@ class OnboardRequirements extends Component {
 }
 const mapStateToProps = state => {
   return {
-    ingredients: state.ingredients,
+    ingredients: state.ingredientSearch,
     requirements: state.requirements
   }
 }
