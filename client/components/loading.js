c411da00e434ca1e26242ebb41b0d85eb4ece141
lgrees <lukasgrees@gmail.com>
2018-11-27 10:03:03 -0600

    adds semantic loading template

--- client/components/loading.js
+++ client/components/loading.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import {Dimmer, Loader, Image, Segment} from 'semantic-ui-react'
+export default function Loading() {
+  return (
+    <div>
+      <Segment>
+        <Dimmer active>
+          <Loader>Loading</Loader>
+        </Dimmer>
 
-export default function Loading () {
-  return <div>Loading...</div>
+        <Image src="/images/wireframe/short-paragraph.png" />
+      </Segment>
+
+      <Segment>
+        <Dimmer active inverted>
+          <Loader inverted>Loading</Loader>
+        </Dimmer>
+
+        <Image src="/images/wireframe/short-paragraph.png" />
+      </Segment>
+    </div>
+  )
 }
