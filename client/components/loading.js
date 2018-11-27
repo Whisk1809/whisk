2391308d2f45278ae03d56e3009ba6fa82ebd000
lgrees <lukasgrees@gmail.com>
2018-11-27 10:05:41 -0600

    adds a fancy loader

--- client/components/loading.js
+++ client/components/loading.js
@@ -3,21 +3,11 @@ import {Dimmer, Loader, Image, Segment} from 'semantic-ui-react'
 export default function Loading() {
   return (
     <div>
-      <Segment>
-        <Dimmer active>
-          <Loader>Loading</Loader>
-        </Dimmer>
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
 
-        <Image src="/images/wireframe/short-paragraph.png" />
-      </Segment>
-
-      <Segment>
-        <Dimmer active inverted>
-          <Loader inverted>Loading</Loader>
-        </Dimmer>
-
-        <Image src="/images/wireframe/short-paragraph.png" />
-      </Segment>
+      <Image src="/images/wireframe/short-paragraph.png" />
     </div>
   )
 }
