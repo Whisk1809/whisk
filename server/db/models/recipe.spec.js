a83b2ecddb92ed0eacf4124458cf749af1cc242e
lgrees <lukasgrees@gmail.com>
2018-11-27 10:32:10 -0600

    adds skeleton of desired tests

--- server/db/models/recipe.spec.js
+++ server/db/models/recipe.spec.js
@@ -10,12 +10,54 @@ describe('Recipe model', () => {
     const recipe = Recipe.build({
       title: 'Grilled Cheese',
       description: 'A really mediocre sandwich',
-      prepTime: '10 minutes',
+      prepTime: '10 minutes'
     })
     expect(recipe.title).to.equal('Grilled Cheese')
     expect(recipe.description).to.equal('A really mediocre sandwich')
     expect(recipe.prepTime).to.equal('10 minutes')
     expect(recipe.imageUrl).to.equal('default-recipe.jpg')
+  })
+  it('has a class method which returns popular recipes', () => {
+    expect()
+  })
+  it('has a class method which returns trending recipes', () => {
+    expect()
+  })
+  it('has a class method which returns new recipes based on the user ID')
+  describe('popular recipes class method', () => {
+    it('returns exactly 15 recipes', () => {
+      expect()
+    })
+    it('returns the most popular recipes based on all time absolute like count', () => {
+      expect()
+    })
+  })
+  describe('new recipes class method', () => {
+    it('returns exactly 15 recipes', () => {
+      expect()
+    })
+    it('gives different content based on the user', () => {
+      expect()
+    })
+    it('returns recipes that the user has not interacted with (via favorite recipes or via preferences)', () => {
+      expect()
+    })
+  })
+  describe('trending recipes class method', () => {
+    it('returns exactly 15 recipes', () => {
+      expect()
+    })
+  })
+  describe('recommended recipes class method', () => {
+    it('returns exactly 15 recipes', () => {
+      expect()
+    })
+    it('gives different content based on the user', () => {
+      expect()
+    })
+    it('returns recipes that the user has not interacted with (via favorite recipes or via preferences)', () => {
+      expect()
+    })
   })
 
   it('title and directions validations', async () => {
