1622e04e08b99a009d653fffd05ced4c6c3cb77f
Lukas Rees <lukasgrees@gmail.com>
2018-11-28 15:26:48 -0600

    Update recipeSearch.js

--- server/api/recipeSearch.js
+++ server/api/recipeSearch.js
@@ -3,15 +3,10 @@ const {Recipe} = require('../db/models')
 
 module.exports = router
 
+
 router.get('/', async (req, res, next) => {
   try {
-    const recipes = await Recipe.findAll({
-      where: {
-        title: {
-          $iLike: req.query.findRecipe + '%'
-        }
-      }
-    })
+    const recipes = await Recipe.search(req.query.findRecipe)
     res.json(recipes)
   } catch (err) {
     console.error(err)
